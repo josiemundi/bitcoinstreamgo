d25a79d7c274f74b777a36572e929502c44c9194
josiemundi <johannasaladas@gmail.com>
2019-11-14 09:15:26 +0000

    Update bitcoinstream.go
    Added struct and unmarshal

--- bitcoinstream.go
+++ bitcoinstream.go
@@ -1,11 +1,51 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/rgamba/evtwebsocket"
 	"log"
 	"time"
-
-	"github.com/rgamba/evtwebsocket"
 )
+
+type Transaction struct {
+	Op string `json:"op"`
+	X  struct {
+		LockTime int `json:"lock_time"`
+		Ver      int `json:"ver"`
+		Size     int `json:"size"`
+		Inputs   []struct {
+			Sequence int64 `json:"sequence"`
+			PrevOut  struct {
+				Spent   bool   `json:"spent"`
+				TxIndex int    `json:"tx_index"`
+				Type    int    `json:"type"`
+				Addr    string `json:"addr"`
+				Value   int    `json:"value"`
+				N       int    `json:"n"`
+				Script  string `json:"script"`
+			} `json:"prev_out"`
+			Script string `json:"script"`
+		} `json:"inputs"`
+		Time      int    `json:"time"`
+		TtxIndex  int    `json:"ttx_index"`
+		VinSz     int    `json:"vin_sz"`
+		Hash      string `json:"hash"`
+		VoutSz    int    `json:"vout_sz"`
+		RelayedBy string `json:"relayed_by"`
+		Out       []struct {
+			Spent   bool   `json:"spent"`
+			TxIndex int    `json:"tx_index"`
+			Type    int    `json:"type"`
+			Addr    string `json:"addr"`
+			Value   int    `json:"value"`
+			N       int    `json:"n"`
+			Script  string `json:"script"`
+		} `json:"out"`
+	} `json:"x"`
+}
+
+
 
 func main() {
 	c := evtwebsocket.Conn{
@@ -17,7 +57,21 @@ func main() {
 
 		// When a message arrives
 		OnMessage: func(msg []byte, w *evtwebsocket.Conn) {
-			log.Printf("OnMessage: %s\n", msg)
+			//log.Printf("OnMessage: %s\n", msg)
+			//fmt.Println(msg)
+			fmt.Printf("MESSAGE: %s\n", msg)
+			var transact Transaction
+			//data := []byte(msg)
+			err := json.Unmarshal(msg, &transact)
+			if err == nil {
+				//fmt.Printf("%s", msg)
+				fmt.Printf("INPUTS:\n")
+				for k, v := range transact.X.Inputs {
+					//fmt.Println(parsed["inputs"])
+					fmt.Println(k, v)
+				}
+				fmt.Println("")
+			}
 		},
 
 		// When the client disconnects for any reason
