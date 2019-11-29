15ed1fb3b1353661f2c86d25d93a838ef2814b6e
josiemundi <johannasaladas@gmail.com>
2019-11-29 10:59:02 +0000

    Update bitcoinstream.go

--- bitcoinstream.go
+++ bitcoinstream.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/rgamba/evtwebsocket"
 	"log"
-	"time"
 )
 
 type Transaction struct {
@@ -48,69 +47,70 @@ type Transaction struct {
 
 
 func main() {
-	c := evtwebsocket.Conn{
 
-		// When connection is established
-		OnConnected: func(w *evtwebsocket.Conn) {
-			log.Println("Connected")
-		},
+	for {
+		c := evtwebsocket.Conn{
 
-		// When a message arrives
-		OnMessage: func(msg []byte, w *evtwebsocket.Conn) {
-			//log.Printf("OnMessage: %s\n", msg)
-			//fmt.Println(msg)
-			fmt.Printf("MESSAGE: %s\n", msg)
-			var transact Transaction
-			//data := []byte(msg)
-			err := json.Unmarshal(msg, &transact)
-			if err == nil {
-				//fmt.Printf("%s", msg)
-				fmt.Printf("INPUTS:\n")
-				for k, v := range transact.X.Inputs {
-					//fmt.Println(parsed["inputs"])
-					fmt.Println(k, v)
+			// When connection is established
+			OnConnected: func(w *evtwebsocket.Conn) {
+				log.Println("Connected")
+			},
+
+			// When a message arrives
+			OnMessage: func(msg []byte, w *evtwebsocket.Conn) {
+				//log.Printf("OnMessage: %s\n", msg)
+				//fmt.Println(msg)
+				fmt.Printf("MESSAGE: %s\n", msg)
+				var transact Transaction
+				//data := []byte(msg)
+				err := json.Unmarshal(msg, &transact)
+				if err == nil {
+					//fmt.Printf("%s", msg)
+					fmt.Printf("INPUTS:\n")
+					for k, v := range transact.X.Inputs {
+						//fmt.Println(parsed["inputs"])
+						fmt.Println(k, v)
+					}
+					fmt.Println("")
 				}
-				fmt.Println("")
-			}
-		},
+			},
 
-		// When the client disconnects for any reason
-		OnError: func(err error) {
-			log.Printf("** ERROR **\n%s\n", err.Error())
-		},
+			// When the client disconnects for any reason
+			OnError: func(err error) {
+				log.Printf("** ERROR **\n%s\n", err.Error())
+			},
 
-		// This is used to match the request and response messagesP>termina
-		MatchMsg: func(req, resp []byte) bool {
-			return string(req) == string(resp)
-		},
+			// This is used to match the request and response messagesP>termina
+			MatchMsg: func(req, resp []byte) bool {
+				return string(req) == string(resp)
+			},
 
-		// Auto reconnect on error
-		Reconnect: true,
+			// Auto reconnect on error
+			Reconnect: true,
 
-		// Set the ping interval (optional)
-		PingIntervalSecs: 5,
+			// Set the ping interval (optional)
+			PingIntervalSecs: 5,
 
-		// Set the ping message (optional)
-		PingMsg: []byte("PING"),
+			// Set the ping message (optional)
+			PingMsg: []byte("PING"),
+		}
+
+		// Connect
+		err := c.Dial("wss://ws.blockchain.info/inv", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		outmsg := evtwebsocket.Msg{
+			Body: []byte("{\"op\":\"unconfirmed_sub\"}"),
+			Callback: func(resp []byte, w *evtwebsocket.Conn) {
+				log.Printf("[%d] Callback: %s\n", 0, resp)
+			},
+		}
+
+		//err = c.Send(msg)
+		if err := c.Send(outmsg); err != nil {
+			log.Println("Unable to send: ", err.Error())
+		}
 	}
-
-	// Connect
-	err := c.Dial("wss://ws.blockchain.info/inv", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	outmsg := evtwebsocket.Msg{
-		Body: []byte("{\"op\":\"unconfirmed_sub\"}"),
-		Callback: func(resp []byte, w *evtwebsocket.Conn) {
-			log.Printf("[%d] Callback: %s\n", 0, resp)
-		},
-	}
-
-	//err = c.Send(msg)
-	if err := c.Send(outmsg); err != nil {
-		log.Println("Unable to send: ", err.Error())
-	}
-
-	time.Sleep(time.Second * 20)
 }
